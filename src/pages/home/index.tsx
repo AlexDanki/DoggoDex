daa70817cc12c50e5cb24ec5fbe7d400d411a61d
Alexsandro Duarte <pocmusic321@gmail.com>
2025-08-19 23:50:57 -0300

    trabalhando com rotas e aplicando page's home e layout

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+
+
+export function Home(){
+    return(
+        <div>
+            <table>
+                <thead>
+                    <th>Nome</th>
+                    <th>Raça</th>
+                    <th>Temperamento</th>
+                </thead>
+            </table>
+        </div>
+    )
+}
